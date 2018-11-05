470fa5acbd88fcac0499e71db08111f0449a8021
wangxiaoqing <xiaoqingwang@126.com>
2018-11-05 16:28:35 +0800

    add test

---
+++ app/src/sagas/test/index.js
@@ -0,0 +1,37 @@
+import { put,takeLatest,call} from 'redux-saga/effects';
+// import {delay} from 'redux-saga';
+import {
+  test_page_getproduct,
+
+} from '../../actions';
+
+import {callthen,page_getproduct_request,page_getproduct_result} from '../pagination';
+// import { goBack } from 'react-router-redux';//https://github.com/reactjs/react-router-redux
+import config from '../../env/config.js';
+import store from '../../env/store';
+// import  {
+//   getrandom
+// } from '../test/bmsdata.js';\
+
+const callfn_page_getproduct = (payload)=>{
+  return store.dispatch(callthen(page_getproduct_request,page_getproduct_result,payload));
+}
+
+export function* testflow() {
+  yield takeLatest(`${test_page_getproduct}`, function*(action) {
+    const query = {};
+    const sort = {};
+    const offset = 0;
+    const pagenumber = 10;
+    const result = yield call(callfn_page_getproduct,{
+        query: query,
+        options: {
+            sort: sort,
+            offset: offset,
+            limit: pagenumber,
+        }
+    });
+    console.log(result);
+  });
+
+}
