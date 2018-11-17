736fe23c9276cf420e6de27c4555cbf9b6cd514d
Jiaowenhui <523947655@qq.com>
2018-11-18 01:21:56 +0800

    no message

--- app/src/components/main/good.js
+++ app/src/components/main/good.js
@@ -5,68 +5,103 @@ import React from 'react';
 import { connect } from 'react-redux';
 // import {NavBar} from 'antd-mobile';
 import { withRouter } from 'react-router-dom';
+import { Carousel, WingBlank, WhiteSpace, List, Modal } from 'antd-mobile';
+
 import "./good.css";
+const Item = List.Item;
+const Brief = Item.Brief;
 
 //顶部搜索
-let GoodsImg = (props)=> {
-    let back=()=>{
-        props.history.goBack();
-    }
-    return (
-        <div className="goodsinfoHead">
-            <div className="goodImg">
-                <a className="backLnk"><i className="icon iconfont icon-Left" onClick={back} /></a>
-                <a className="shoppingcar"><i className="icon iconfont icon-gouwuche" /></a>
-                <a className="moreLnk"><i className="icon iconfont icon-gengduo-tianchong" /></a>
-                <img src="./img/1.png" />
-            </div>
-            <div className="data">
-                <div className="nn">
-                    <div className="left">
-                        <span className="price">￥260.00 - 500.00</span>
-                        <span className="name">爱上门血压仪</span>
-                    </div>
-                    <div className="right">
-                        <span className="shear"><i className="icon iconfont icon-fenxiang"/> 分享</span>
-                    </div>
-                </div>
-                <div className="nm">
-                    <span>快递：20.00元</span>
-                    <span>月销：23243件</span>
-                    <span>常州</span>
-                </div>
-            </div>
-        </div>
-    );
-};
-GoodsImg = withRouter(GoodsImg);
+// let GoodsImg = (props)=> {
+//     let back=()=>{
+//         props.history.goBack();
+//     }
+//     return (
+        
+//     );
+// };
+// GoodsImg = withRouter(GoodsImg);
 //产品详情底部
-let GoodsFoot = (props)=> {
-    return (
-        <div className="goodsFoot">
-            <div className="leftlnk">
-                <div><i className="icon iconfont icon-dianpu" /><span>店铺</span></div>
-                <div><i className="icon iconfont icon-kefu" /><span>客服</span></div>
-                <div><i className="icon iconfont icon-shoucang1" /><span>收藏</span></div>
-            </div>
-            <div className="sublnk">
-                <div>加入购物车</div>
-                <div>立刻购买</div>
-            </div>
-        </div>
-    );
-};
+// let GoodsFoot = (props)=> {
+//     let poppush=(url)=>{
+//         props.history.push(url);
+//     }
+//     return (
+        
+//     );
+// };
+// GoodsFoot = withRouter(GoodsFoot);
 //产品选项组
-let GoodsBtnList = (props)=> {
-    let poppush =(url)=>{
-        props.history.push(url);
+class GoodsBtnList extends React.Component {
+    constructor(props) {  
+        super(props);
+        this.state = {
+            canshubox: false,
+            xuankuan : false
+        };
+    } 
+    poppush(url){
+        this.props.history.push(url);
     }
-    return (
-        <div className="goodsBtnList">
-            <div onClick={()=>{poppush("/goodinfo/canshu")}}><span>参数</span><i className="icon iconfont icon-xiangyou" /></div>
-            <div onClick={()=>{poppush("/goodinfo/sel")}}><span>选择</span><i className="icon iconfont icon-xiangyou" /></div>
-        </div>
-    );
+    onClose = key =>() => {
+        this.setState({
+          [key]: false,
+        });
+    }
+    showModal = key => (e) => {
+        e.preventDefault(); // 修复 Android 上点击穿透
+        this.setState({
+          [key]: true,
+        });
+    }
+    render() {
+        return (
+            <div className="goodsBtnList">
+                <div onClick={this.showModal('canshubox')}><span>参数</span><i className="icon iconfont icon-xiangyou" /></div>
+                <div onClick={this.showModal('xuankuan')}><span>选择</span><i className="icon iconfont icon-xiangyou" /></div>
+                <Modal
+                    popup
+                    visible={this.state.canshubox}
+                    onClose={this.onClose('canshubox')}
+                    animationType="slide-up"
+                    >
+                    <List 
+                        renderHeader={() => <div>参数</div>}
+                        className="popup-list">
+                        <Item extra={'参数1值参数1值'}>参数1</Item>
+                        <Item extra={'参数1值参数2值'}>参数2</Item>
+                        <Item extra={'参数1值参数3值'}>参数3</Item>
+                        <Item extra={'参数1值参数4值'}>参数4</Item>
+                    </List>
+                </Modal>
+                <Modal
+                    popup
+                    visible={this.state.xuankuan}
+                    onClose={this.onClose('xuankuan')}
+                    animationType="slide-up"
+                    className="selprotype"
+                    >
+                    <List 
+                        renderHeader={() => <div>选款</div>}
+                        className="popup-list">
+                        <div className="seltype"><span>款式1</span><span>款式1</span></div>
+                    </List>
+                    <div className="selprotypesetnum">
+                        <span>数量</span>
+                        <div className="addtools">
+                            <div className="del">-</div>
+                            <input type="number" default="1" value="1" onChange={()=>{}} />
+                            <div className="add">+</div>
+                        </div>
+                    </div>
+                    <div className="selprotypesublnk">
+                        <div onClick={()=>{this.onClose('canshubox')}}>加入购物车</div>
+                        <div onClick={()=>{this.poppush('/order/createorder')}}>立刻购买</div>
+                    </div>
+                </Modal>
+            </div>
+        );
+    }
 };
 GoodsBtnList = withRouter(GoodsBtnList);
 
@@ -108,21 +143,139 @@ let GoodText = (props)=> {
 export class Page extends React.Component {
 
     constructor(props) {  
-        super(props);  
-        this.state = {
-            p: 0,
-            innerHeight : window.innerHeight
+        super(props);
+        this.state = {
+            data: ['1', '2', '3'],
+            canshubox: false,
+            xuankuan : false
         };
     } 
-
+    poppush(url){
+        this.props.history.push(url);
+    }
+    onClose = key =>() => {
+        this.setState({
+          [key]: false,
+        });
+    }
+    showModal = key => (e) => {
+        e.preventDefault(); // 修复 Android 上点击穿透
+        this.setState({
+          [key]: true,
+        });
+    }
+    back(){
+        this.props.history.goBack();
+    }
     render() {
         return (
             <div className="goodInfoPage" style={{height: window.innerHeight+"px"}}>
-                <GoodsImg />
-                <GoodsBtnList />
+                <div className="goodsinfoHead">
+                    <div className="goodImg">
+                        <a className="backLnk"><i className="icon iconfont icon-Left" onClick={this.back} /></a>
+                        <a className="shoppingcar" onClick={()=>{this.poppush("/shopping/car")}}><i className="icon iconfont icon-gouwuche" /></a>
+                        <a className="moreLnk"><i className="icon iconfont icon-gengduo-tianchong" /></a>
+                        <WingBlank>
+                            <Carousel
+                              autoplay={false}
+                              infinite
+                              beforeChange={(from, to) => console.log(`slide from ${from} to ${to}`)}
+                              afterChange={index => console.log('slide to', index)}
+                            >
+                              {this.state.data.map(val => (
+                                <a
+                                  key={val}
+                                  style={{ display: 'inline-block', width: '100%' }}
+                                >
+                                  <img
+                                    src="./img/1.png"
+                                    alt=""
+                                    style={{ width: '100%', verticalAlign: 'top' }}
+                                    onLoad={() => {
+                                      // fire window resize event to change height
+                                      window.dispatchEvent(new Event('resize'));
+                                      this.setState({ imgHeight: 'auto' });
+                                    }}
+                                  />
+                                </a>
+                              ))}
+                            </Carousel>
+                          </WingBlank>
+                    </div>
+                    <div className="data">
+                        <div className="nn">
+                            <div className="left">
+                                <span className="price">￥260.00 - 500.00</span>
+                                <span className="name">爱上门血压仪</span>
+                            </div>
+                            <div className="right">
+                                <span className="shear"><i className="icon iconfont icon-fenxiang"/> 分享</span>
+                            </div>
+                        </div>
+                        <div className="nm">
+                            <span>快递：20.00元</span>
+                            <span>月销：23243件</span>
+                            <span>常州</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="goodsBtnList">
+                    <div onClick={this.showModal('canshubox')}><span>参数</span><i className="icon iconfont icon-xiangyou" /></div>
+                    <div onClick={this.showModal('xuankuan')}><span>选择</span><i className="icon iconfont icon-xiangyou" /></div>
+                    <Modal
+                        popup
+                        visible={this.state.canshubox}
+                        onClose={this.onClose('canshubox')}
+                        animationType="slide-up"
+                        >
+                        <List 
+                            renderHeader={() => <div>参数</div>}
+                            className="popup-list">
+                            <Item extra={'参数1值参数1值'}>参数1</Item>
+                            <Item extra={'参数1值参数2值'}>参数2</Item>
+                            <Item extra={'参数1值参数3值'}>参数3</Item>
+                            <Item extra={'参数1值参数4值'}>参数4</Item>
+                        </List>
+                    </Modal>
+                    <Modal
+                        popup
+                        visible={this.state.xuankuan}
+                        onClose={this.onClose('xuankuan')}
+                        animationType="slide-up"
+                        className="selprotype"
+                        >
+                        <List 
+                            renderHeader={() => <div>选款</div>}
+                            className="popup-list">
+                            <div className="seltype"><span>款式1</span><span>款式1</span></div>
+                        </List>
+                        <div className="selprotypesetnum">
+                            <span>数量</span>
+                            <div className="addtools">
+                                <div className="del">-</div>
+                                <input type="number" default="1" value="1" onChange={()=>{}} />
+                                <div className="add">+</div>
+                            </div>
+                        </div>
+                        <div className="selprotypesublnk">
+                            <div onClick={()=>{this.onClose('canshubox')}}>加入购物车</div>
+                            <div onClick={()=>{this.onClose('canshubox')}}>立刻购买</div>
+                        </div>
+                    </Modal>
+                </div>
                 <PingjiaList />
                 <GoodText />
-                <GoodsFoot />
+                <div className="goodsFoot">
+                    <div className="leftlnk">
+                        <div><i className="icon iconfont icon-dianpu" /><span>店铺</span></div>
+                        <div><i className="icon iconfont icon-kefu" /><span>客服</span></div>
+                        <div><i className="icon iconfont icon-shoucang1" /><span>收藏</span></div>
+                    </div>
+                    <div className="sublnk">
+                        <div onClick={()=>{this.onClose('canshubox')}}>加入购物车 </div>
+                        <div onClick={()=>{this.onClose('canshubox')}}>立刻购买</div>
+                    </div>
+                </div>
             </div>
         )
     }
